72e85b41dec4f24b43ae19e9e68f1988f207321d
Miguel Lammens <justin.bugtong@gmail.com>
2020-04-17 21:03:41 +0200

    passport toevoegen

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ let app = express();
 
 //MONGOOSE
 const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/transfer', {
   useNewUrlParser: true, useUnifiedTopology: true
 });
--- routes/api/v1/transfer.js
+++ routes/api/v1/transfer.js
@@ -4,7 +4,9 @@ const traController = require('../../../controllers/api/v1/transfer');
 
 router.get("/",traController.getAll);
 
+//post first transfer
 router.post("/",traController.create);
+
 router.put("/",traController.update);
 
 module.exports= router;
